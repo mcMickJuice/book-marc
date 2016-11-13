1bd99375a641eadfd2ea39b68626e2dec99da936
Mike Joyce <mikejoyce19@gmail.com>
2016-11-12 19:44:22 -0600

    added store to global in dev

--- src/client-app/redux/configureStore.js
+++ src/client-app/redux/configureStore.js
@@ -1,10 +1,16 @@
-import {createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
 import thunk from 'redux-thunk'
 
+
+
 const configureStore = (defaultState = {}) => {
     const enhancers = applyMiddleware(thunk)
-    return createStore(rootReducer, defaultState, enhancers)
+    var store = createStore(rootReducer, defaultState, enhancers)
+    if (process.env.NODE_ENV === 'development') {
+        window._store = store;
+    }
+    return store;
 }
 
 export default configureStore
