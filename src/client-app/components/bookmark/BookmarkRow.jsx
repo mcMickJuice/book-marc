43bc2c6259011ab5ffe3e2eabbdf1e94c517d21f
Mike Joyce <mikejoyce19@gmail.com>
2017-02-19 18:28:40 -0600

    removed BookmarkListing. redid BookmarkRow. styled some stuff

--- src/client-app/components/bookmark/BookmarkRow.jsx
+++ src/client-app/components/bookmark/BookmarkRow.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import {bookmarkShapeWithReview} from '../../common/bookmarkShape'
-import {Link} from 'react-router'
+import { bookmarkShapeWithReview } from '../../common/bookmarkShape'
+import { Link } from 'react-router'
 import BookmarkUrlLink from './BookmarkUrlLink'
+import Date from '../../elements/Date'
+import Card from '../../elements/Card'
+import * as css from '../../styles/bookmark-row'
 
 const BookmarkRow = ({bookmark}) => {
     const {title, id, url, rating} = bookmark;
 
     return (
-        <tr>
-            <td><Link to={`/bookmark/${id}`}>{title}</Link></td>
-            <td>
+        <Card className='bm-bookmark-row'>
+            <Link to={`/bookmark/${id}`} className="bm-bookmark-row__title">{title}</Link>
+            <div className="bm-bookmark-row__date">
+                <Date date={bookmark.createdDate} />
+            </div>
+            <div className="bm-bookmark-row__rating">
                 {rating ? rating : 'No Rating'}
-            </td>
-        </tr>
+            </div>
+        </Card>
     )
 }
 
